4474162db4a8ad2b55ef9d367d6316df7599a622
ps13g <saketh.2004@gmail.com>
2023-02-13 13:02:34 -0700

    Fixes

--- UNit-5/DragonTester.java
+++
@@ -1,8 +0,0 @@
-public class DragonTester
-{
-    public static void main(String[] args)
-    {
-        // Create a Dragon here to test out the Dragon class!
-    }
-}
-
--- UNit-5/DragonTester2.java
+++
@@ -1,7 +0,0 @@
-public class DragonTester2
-{
-    public static void main(String[] args)
-    {
-        // Start here!
-    }
-}
